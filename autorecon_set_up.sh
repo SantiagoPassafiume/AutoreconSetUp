d751d440159f2bd8b349b1cad2acd369d98e7b36
Santiago Passafiume <santiagopassafiume@protonmail.com>
2021-12-31 11:57:44 -0500

    add check to see if script is running on Kali

--- autorecon_set_up.sh
+++ autorecon_set_up.sh
@@ -7,15 +7,29 @@ sudo apt-get -y install python3-pip
 sudo apt-get -y install python3-venv
 
 
-# Change "~/.zshrc" to "~/.bashrc" if you're not using Kali.
-source ~/.zshrc
+# Check if you are using Kali or not
+COMMAND=$(uname -a);
 
+if [[ $COMMAND =~ "kali" ]];
+then
+        SCRIPT=~/.zshrc
+else
+        SCRIPT=~/.bashrc
+fi
+source $SCRIPT
+
+
+
+# Set up pipx
 sudo python3 -m pip install --user pipx
 sudo python3 -m pipx ensurepath
 
+# Install dependencies
 sudo apt-get -y install seclists curl enum4linux feroxbuster impacket-scripts nbtscan nikto nmap onesixtyone oscanner redis-tools smbclient smbmap snmp sslscan sipvicious tnscmd10g whatweb wkhtmltopdf
 
+# Install autorecon
 pipx install git+https://github.com/Tib3rius/AutoRecon.git
+
 
 
 # If you want to run autorecon using sudo, you need to use one of the following examples:
